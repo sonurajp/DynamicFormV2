af455bafd90692fd9fa1847919bebe7e4875d553
Sonuraj <sonuraj@litmus7.com>
2020-10-26 19:41:42 +0530

    version 3

--- src/Registration/DynamicTextField.js
+++ src/Registration/DynamicTextField.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { Paper, Grid, Container } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
-import { IconButton } from '@material-ui/core';
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import DateFnsUtils from '@date-io/date-fns';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { Paper, Grid, Container } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import { IconButton } from "@material-ui/core";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import DateFnsUtils from "@date-io/date-fns";
 import {
-    MuiPickersUtilsProvider,
-    KeyboardDatePicker,
-} from '@material-ui/pickers';
-    
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+
 const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(3),
-            width: '26ch',
-        },
+  root: {
+    "& > *": {
+      margin: theme.spacing(3),
+      width: "26ch",
     },
-    input: {
-        display: "none"
-    }
+  },
+  input: {
+    display: "none",
+  },
 }));
 
-const DynamicTextField = ({values,  handleChange, handleNext, handleBack }) => {
-    const [inputList, setInputList] = React.useState(values);
-const handleInputChange = (e, index) => {
+const DynamicTextField = ({ values, handleChange, handleNext, handleBack }) => {
+  const [inputList, setInputList] = React.useState(values);
+  const handleInputChange = (e, index) => {
     const { name, value } = e.target;
     const list = [...inputList];
     // list[index][name] = value;
@@ -36,7 +36,7 @@ const handleInputChange = (e, index) => {
   };
 
   // handle click event of the Remove button
-  const handleRemoveClick = index => {
+  const handleRemoveClick = (index) => {
     const list = [...inputList];
     list.splice(index, 1);
     setInputList(list);
@@ -46,74 +46,74 @@ const handleInputChange = (e, index) => {
   const handleAddClick = () => {
     setInputList([...inputList, { firstName: "", lastName: "" }]);
   };
-    const classes = useStyles();
-    const [selectedDate, setSelectedDate] = React.useState(new Date('2020-08-18T21:11:54'));
+  const classes = useStyles();
+  const [selectedDate, setSelectedDate] = React.useState(
+    new Date("2020-08-18T21:11:54")
+  );
 
-    const handleDateChange = (date) => {
-        setSelectedDate(date);
-    };
-    return (
-        <div>
-            
-                    <Grid container direction="column" spacing={3}>   
-                        <Grid item xs={12} className={classes.root}>
-                            {inputList.map((x, i) =>{
-                            return (
-                            <>
-                            <TextField
-                                color='secondary'
-                                required
-                                id="outlined-required"
-                                label="Full Name"
-                                variant="outlined"
-                              
-                                onChange={e => handleInputChange(e, i,'fullName')}
-                                defaultValue={x.fullName}
-                            />
-                            <TextField
-                                color='secondary'
-                                required
-                                id="outlined-required"
-                                label="Designation"
-                                variant="outlined"
-                                name='designation'
-                                onChange={e => handleInputChange(e, i,'designation')}
-                                defaultValue={x.designation}
-
-                            />
-                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                                <KeyboardDatePicker
-                                    disableToolbar
-                                    variant="inline"
-                                    format="dd/mm/yyyy"
-                                    margin="normal"
-                                    id="date-Of-joining"
-                                    label="Date Of joining"
-                                    value={selectedDate}
-                                    onChange={handleDateChange}
-                                    KeyboardButtonProps={{
-                                        'aria-label': 'change date',
-                                    }}
-                                />
-                            </MuiPickersUtilsProvider>
-                            <div className="btn-box">
-                { <button
-                    className="mr10"
-                    onClick={() => handleRemoveClick(i)}>Remove</button>}
-                {inputList.length - 1 === i && <button onClick={handleAddClick}>Add</button>}
-            </div> 
-                            </>
-                            );
-                        })}
-                          
-                        </Grid>
-
-                        
-                    </Grid>
-                
-
-        </div>
-    )
-
-}
-export default DynamicTextField
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+  return (
+    <div>
+      <Grid container direction="column" spacing={3}>
+        <Grid item xs={12} className={classes.root}>
+          {inputList.map((x, i) => {
+            return (
+              <>
+                <TextField
+                  color="secondary"
+                  required
+                  id="outlined-required"
+                  label="Full Name"
+                  variant="outlined"
+                  onChange={(e) => handleInputChange(e, i, "fullName")}
+                  defaultValue={x.fullName}
+                />
+                <TextField
+                  color="secondary"
+                  required
+                  id="outlined-required"
+                  label="Designation"
+                  variant="outlined"
+                  name="designation"
+                  onChange={(e) => handleInputChange(e, i, "designation")}
+                  defaultValue={x.designation}
+                />
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                  <KeyboardDatePicker
+                    disableToolbar
+                    variant="inline"
+                    format="dd/mm/yyyy"
+                    margin="normal"
+                    id="date-Of-joining"
+                    label="Date Of joining"
+                    value={selectedDate}
+                    onChange={handleDateChange}
+                    KeyboardButtonProps={{
+                      "aria-label": "change date",
+                    }}
+                  />
+                </MuiPickersUtilsProvider>
+                <div className="btn-box">
+                  {
+                    <button
+                      className="mr10"
+                      onClick={() => handleRemoveClick(i)}
+                    >
+                      Remove
+                    </button>
+                  }
+                  {inputList.length - 1 === i && (
+                    <button onClick={handleAddClick}>Add</button>
+                  )}
+                </div>
+              </>
+            );
+          })}
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+export default DynamicTextField;
--- src/Registration/SignatoryDetails.js
+++ src/Registration/SignatoryDetails.js
@@ -1,135 +1,142 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { Paper, Grid, Container } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
-import { IconButton } from '@material-ui/core';
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import DateFnsUtils from '@date-io/date-fns';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { Paper, Grid, Container } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import { IconButton } from "@material-ui/core";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import DateFnsUtils from "@date-io/date-fns";
 import {
-    MuiPickersUtilsProvider,
-    KeyboardDatePicker,
-} from '@material-ui/pickers';
-import DynamicTextField from './DynamicTextField'
-import AddIcon from '@material-ui/icons/Add';
-import SigCss from './SignatoryDetails.scss'
-import HighlightOffIcon from '@material-ui/icons/HighlightOff';
-import SignatoryTextField from './SignatoryTextField'
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import AddIcon from "@material-ui/icons/Add";
+import SignatoryScss from "./SignatoryDetails.scss";
+import HighlightOffIcon from "@material-ui/icons/HighlightOff";
 const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(3),
-            width: '26ch',
-        },
+  root: {
+    "& > *": {
+      margin: theme.spacing(2),
+      // width: "20ch",
     },
-    input: {
-        display: "none"
-    }
+  },
+  input: {
+    display: "none",
+  },
 }));
-var fieldPopNumber=0;
-const SignatoryDetails = ({values,  handleChange, handleNext, handleBack,setStateValue,stateValue }) => {
-   
-    const classes = useStyles();
-    const [selectedDate, setSelectedDate] = React.useState(new Date('2020-08-18T21:11:54'));
-const popDiv=()=>{
-    fieldPopNumber=fieldPopNumber+1;
-    setStateValue(fieldPop =>[...fieldPop,fieldPopNumber])
-}
-    const handleDateChange = (date) => {
-        setSelectedDate(date);
-    };
-    const removeDiv=(value,index)=>{
-        const teampArray=[...stateValue];
-        teampArray.splice(index,1);
-        setStateValue(teampArray);
-    }
-    return (
-        <div>
-            <Container maxWidth='lg' >
-                <Paper elevation={1} style={{ padding: "16px" }}>
-                    <Grid container direction="column" spacing={3}>
-                        <Grid item xs={12}>
-                            <Typography variant="h4">
-                                Your Signatory Details
-                             </Typography>
-                        </Grid>
-                        <Grid item xs={12} className={classes.root}>
-                        <SignatoryTextField values={values} handleChange={handleChange}/>
-                        </Grid>
-                        <Grid item xs={12} className={classes.root}>
-                        {stateValue.map((item,index)=>{
-                    return( 
-                              <React.Fragment key={index}>
-                                 <TextField
-                                     color='secondary'
-                                     required
-                                    id="outlined-required"
-                                     label="Full Name"
-                                    defaultValue=""
-                                    variant="outlined"
-                                    onChange={handleChange(`${index}fullNameSub`)}
-                                    defaultValue={values[`${index}fullNameSub`]}
-                                />
-                                <TextField
-                                    color='secondary'
-                                    required
-                                    id="outlined-required"
-                                    label="Designation"
-                                    defaultValue=""
-                                    variant="outlined"
-                                    onChange={handleChange(`${index}designationUb`)}
-                                    defaultValue={values[`${index}designationUb`]}
+const SignatoryDetails = ({
+  handleNext,
+  handleBack,
+  handleDateChange,
+  handleInputChange,
+  inputList,
+  setInputList,
+  selectedDate,
+}) => {
+  const classes = useStyles();
 
-                                />
-                                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                                            <KeyboardDatePicker
-                                                disableToolbar
-                                                variant="inline"
-                                                format="dd/mm/yyyy"
-                                                margin="normal"
-                                                id="date-Of-joining"
-                                                label="Date Of joining"
-                                                value={selectedDate}
-                                                onChange={handleDateChange}
-                                                KeyboardButtonProps={{
-                                                    'aria-label': 'change date',
-                                                }}
-                                            />
-                                        </MuiPickersUtilsProvider>
-                                        < IconButton className="HideButton" onClick={()=>removeDiv(item,index)} ><HighlightOffIcon/></IconButton>
-                                        </React.Fragment>
-                                        )
-                                                                })
-                        }
-                        
-                        < IconButton className="ButtonFloat" onClick={popDiv}><AddIcon  />add</IconButton>
-                          
-                           
-                        </Grid>
+  // handle click event of the Remove button
+  const handleRemoveClick = (index) => {
+    const list = [...inputList];
+    list.splice(index, 1);
+    setInputList(list);
+  };
 
-                        <Grid container item >
-                            <Grid xs={4} item>
-                                <IconButton aria-label="back" onClick={() => handleBack()}>
-                                    <ArrowBackIosIcon fontSize="small" />
-                                    <Typography>back</Typography>
-                                </IconButton>
+  // handle click event of the Add button
+  const handleAddClick = () => {
+    setInputList([...inputList, { firstName: "", designation: "" }]);
+  };
 
-                            </Grid>
-                            <Grid container xs={8} item alignItems="flex-start">
-                                <Button variant="contained" color="primary" style={{
-                                    backgroundColor: "#707070"
-                                }} onClick={() => handleNext()}>
-                                    <Typography>Continue to Next</Typography>
-                                </Button>
-                            </Grid>
-                        </Grid>
+  return (
+    <div>
+      <Container maxWidth="md">
+        <Paper elevation={1} style={{ padding: "10px" }}>
+          <Grid container direction="column" spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h4">Your Signatory Details</Typography>
+            </Grid>
+            <Grid item xs={12} className={classes.root}>
+              {inputList.map((x, i) => {
+                return (
+                  <React.Fragment>
+                    <TextField
+                      color="secondary"
+                      name="firstName"
+                      label="Full Name"
+                      value={x.firstName}
+                      onChange={(e) => handleInputChange(e, i)}
+                      variant="outlined"
+                      style={{ width: 200 }}
+                    />
+                    <TextField
+                      color="secondary"
+                      name="designation"
+                      label="designation"
+                      value={x.designation}
+                      onChange={(e) => handleInputChange(e, i)}
+                      variant="outlined"
+                    />
+                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                      <KeyboardDatePicker
+                        disableToolbar
+                        variant="inline"
+                        format="dd/mm/yyyy"
+                        margin="normal"
+                        id="date-Of-joining"
+                        label="Date Of joining"
+                        value={selectedDate}
+                        onChange={handleDateChange}
+                        KeyboardButtonProps={{
+                          "aria-label": "change date",
+                        }}
+                      />
+                    </MuiPickersUtilsProvider>
+                    {inputList.length !== 1 && (
+                      <IconButton
+                        className="HideButton"
+                        onClick={() => handleRemoveClick(i)}
+                      >
+                        <HighlightOffIcon />
+                      </IconButton>
+                    )}
+                    <Grid>
+                      {inputList.length - 1 === i && (
+                        <IconButton onClick={handleAddClick}>
+                          <AddIcon />
+                          add
+                        </IconButton>
+                      )}
                     </Grid>
-                </Paper>
-            </Container>
+                  </React.Fragment>
+                );
+              })}
+            </Grid>
+            <Grid container item>
+              <Grid xs={4} item>
+                <IconButton aria-label="back" onClick={() => handleBack()}>
+                  <ArrowBackIosIcon fontSize="small" />
+                  <Typography>back</Typography>
+                </IconButton>
+              </Grid>
+              <Grid container xs={8} item alignItems="flex-start">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={{
+                    backgroundColor: "#707070",
+                  }}
+                  onClick={() => handleNext()}
+                >
+                  <Typography>Continue to Next</Typography>
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Container>
+    </div>
+  );
+};
 
-        </div>
-    )
-
-}
-export default SignatoryDetails
+export default SignatoryDetails;
